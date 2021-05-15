89fe887ec50df0129887aa0d8f8d1164cdbdcb50
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-15 22:18:30 +0530

    Simple linked list

---
+++ src/main/java/INode.java
@@ -0,0 +1,7 @@
+public interface INode<K> {
+    K getKey();
+    void setKey(K Key);
+
+    INode<K> getNext();
+    void setNext(INode<K> next);
+}
---
+++ src/main/java/MyNode.java
@@ -0,0 +1,27 @@
+public class MyNode<K> implements INode<K> {
+    private K key;
+    private MyNode<K> next;
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public INode<K> getNext() {
+        return next;
+    }
+
+    @Override
+    public void setNext(INode next) {
+        this.next = (MyNode<K>) next;
+    }
+}
