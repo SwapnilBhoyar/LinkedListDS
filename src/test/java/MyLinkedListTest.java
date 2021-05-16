b1bcdecbd409b9aa50a2547a7b2a845d221f85ef
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-17 00:51:41 +0530

    Creating and appending to linked list

--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -15,4 +15,18 @@ public class MyLinkedListTest {
         boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myFirstNode);
         Assertions.assertTrue(result);
     }
+
+    @Test
+    public void given3NumbersWhenAppendedShouldBeAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
 }
